f46cbe36aa708354a933a27d87681d328cc73564
Jason <bjason@gmail.com>
2015-03-10 11:26:01 -0700

    Swift Prototypes and learnings

---
+++ HelloWorld/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sqor Admin on 3/10/15.
+//  Copyright (c) 2015 Sqor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func helloClicked(sender: UIButton) {
+    }
+    
+    
+
+}
+
