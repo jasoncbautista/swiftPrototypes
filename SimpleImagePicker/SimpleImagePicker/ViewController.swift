8569fe750674ff53e1abb736db61fb6ddef7860e
Jason <bjason@gmail.com>
2015-03-13 15:44:26 -0700

    Remove old dead code

--- SimpleImagePicker/SimpleImagePicker/ViewController.swift
+++ SimpleImagePicker/SimpleImagePicker/ViewController.swift
@@ -266,21 +266,12 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     }
     
     
-    
     var postValue = "Sample Post"
     func makePost(){
-        
-        
-        
-        
+    
         println("submitting post")
         
-        // if blank dont submit
-        
-        
-        
-        
-        
+        // TODO: if blank dont submit
         var request = NSMutableURLRequest(URL: NSURL(string: "https://rest-dev.sqor.com/posts")!)
         var session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
@@ -359,11 +350,6 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     }
     
     
-    
-    
-    
-    
-    
     func URLSession(session: NSURLSession, task: NSURLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
         var uploadProgress: Float = Float(totalBytesSent) / Float(totalBytesExpectedToSend)
         println("session \(session) uploaded \(uploadProgress * 100)%.")
@@ -392,11 +378,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         
         // if blank dont submit
         
-        
-        
-        
-        
-        
+
         var request = NSMutableURLRequest(URL: NSURL(string: "https://rest-dev.sqor.com/posts/upload")!)
         var session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
@@ -420,51 +402,11 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         request.addValue("6", forHTTPHeaderField: "sqor-api-version")
         
         var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            println("Response: \(response)")
+      
             var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
-            println("Body: \(strData!)")
-            
-            println("WTF WTF WTF ")
-            
+
             self.uploadActualFile(strData!)
-            println("WTF WTF WTF ")
-            
-            var err: NSError?
-            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
-            
-            
-            
-            
-            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-            if(err != nil) {
-                println(err!.localizedDescription)
-                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                println("XXXX Error could not parse JSON: '\(jsonStr)'")
-                
-                
-                
-            }
-            else {
-                // The JSONObjectWithData constructor didn't return an error. But, we should still
-                // check and make sure that json has a value using optional binding.
-                if let parseJSON = json {
-                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
-                    var success = parseJSON["success"] as? Int
-                    println("Succes: \(success)")
-                }
-                else {
-                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
-                    
-                    
-                    //  println("Error could not parse JSON: \(jsonStr)")
-                    //   print("SUCCESS!!!: ")
-                    //    print(jsonStr)
-                    
-                    
-                    
-                }
-            }
+
         })
         
         task.resume()
